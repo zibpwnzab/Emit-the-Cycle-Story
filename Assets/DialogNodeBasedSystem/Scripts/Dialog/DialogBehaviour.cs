401899be02ab97b49baad6216585f5ef4a550d94
Ekaterina Urmanova <aleremualeremus@gmail.com>
2024-03-03 23:22:28 +0300

    touch dialogue input

--- Assets/DialogNodeBasedSystem/Scripts/Dialog/DialogBehaviour.cs
+++ Assets/DialogNodeBasedSystem/Scripts/Dialog/DialogBehaviour.cs
@@ -277,7 +277,8 @@ namespace cherrydev
         /// <returns></returns>
         private bool CheckNextSentenceKeyCodes()
         {
-            //if (Input.GetTouch(0).type == TouchType.Direct) return true;
+            if (Input.touchCount > 0)
+            if (Input.GetTouch(0).phase == TouchPhase.Ended) return true;
             for (int i = 0; i < nextSentenceKeyCodes.Count; i++)
             { 
                 if (Input.GetKeyDown(nextSentenceKeyCodes[i]))
