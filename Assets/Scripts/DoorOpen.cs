05bc7528d64fdeadeea0549008584310d4306226
Dmitry Korchagin <dmitry.korchagin2019@gmail.com>
2024-05-15 15:16:08 +0300

    lvl 2.5 door fix

--- Assets/Scripts/DoorOpen.cs
+++ Assets/Scripts/DoorOpen.cs
@@ -6,17 +6,19 @@ public class DoorOpen : MonoBehaviour
 {
     [SerializeField] public LaserReceiver signal;
     [SerializeField] public GameObject door;
-    [SerializeField] public GameObject[] plane; 
+    [SerializeField] public GameObject[] plane;
+    [SerializeField] public GameObject dust;
     float y;
     void Update()
     {
         
             if (signal.powered == true && y < 10)
             {
+            dust.gameObject.SetActive(true);
             foreach (GameObject obj in plane)
             {
                 Renderer plane = obj.GetComponent<Renderer>();
-                plane.material.color = new Color(37, 232, 8);
+                plane.material.color = new Color(0, 255, 0);
             }
                 y += 0.05f;
                 transform.position = new Vector2(77.84245f, y);
