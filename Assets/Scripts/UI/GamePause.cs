96ec075a0045b0ed0ad25d6f6d1a8511383dded8
zibpwnzab <d.i.m.p.r.o554@gmail.com>
2025-02-21 15:38:51 +0300

    Pause UPD

--- Assets/Scripts/UI/GamePause.cs
+++ Assets/Scripts/UI/GamePause.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class GamePause : MonoBehaviour
 {
     public GameObject panel;
-    public void pause() 
+    public List<GameObject> objectsToDisable; // Список объектов, которые нужно отключать при паузе
+
+    public void pause()
     {
         Time.timeScale = 0;
         panel.SetActive(true);
+        DisableObjects(); // Отключаем объекты при паузе
     }
 
     public void resume()
-    { 
+    {
         Time.timeScale = 1;
         panel.SetActive(false);
+        EnableObjects(); // Включаем объекты при снятии паузы
+    }
 
+    private void DisableObjects()
+    {
+        foreach (GameObject obj in objectsToDisable)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    private void EnableObjects()
+    {
+        foreach (GameObject obj in objectsToDisable)
+        {
+            obj.SetActive(true);
+        }
     }
 }
